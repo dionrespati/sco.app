fc4f15769d3fbe192b6c26148c41a484e43f1196
Dion Respati <dionrespati@gmail.com>
2021-09-30 06:03:20 +0700

    second commit

--- application/controllers/member/cekVchStk.php
+++ application/controllers/member/cekVchStk.php
@@ -7,33 +7,33 @@ if($result['arraydata'] !== null) {
     $arr = $result['arraydata'][0];
 ?>
 <table class='table table-bordered' width="50%">
-    <thead>
-        <th colspan="2">Data Voucher</th>
-    </thead>
-    <tbody>
-        <tr>
-            <td width="30%">Voucher No</td>
-            <td><?php echo $arr->formno; ?></td>
-        </tr>
-        <tr>
-            <td>Voucher Key</td>
-            <td><?php echo $arr->vchkey; ?></td>
-        </tr>
-        <tr>
-            <td>Status</td>
-            <td><?php echo $arr->ket_status; ?></td>
-        </tr>
-        <tr>
-            <td>Activate ID Member</td>
-            <td><?php echo $arr->activate_dfno; ?></td>
-        </tr>
-    </tbody>
+	<thead>
+		<th colspan="2">Data Voucher</th>
+	</thead>
+	<tbody>
+		<tr>
+			<td width="30%">Voucher No</td>
+			<td><?php echo $arr->formno; ?></td>
+		</tr>
+		<tr>
+			<td>Voucher Key</td>
+			<td><?php echo $arr->vchkey; ?></td>
+		</tr>
+		<tr>
+			<td>Status</td>
+			<td><?php echo $arr->ket_status; ?></td>
+		</tr>
+		<tr>
+			<td>Activate ID Member</td>
+			<td><?php echo $arr->activate_dfno; ?></td>
+		</tr>
+	</tbody>
 </table>
 
 
 <?php
-if($result['response'] == "true") {
-    echo "<input type='button' class='btn btn-mini btn-primary' value='Release Voucher' name='releasebtn' onclick=releaseVch('$arr->formno','$arr->vchkey','$user') />";
-}    
+	if($result['response'] == "true") {
+			echo "<input type='button' class='btn btn-mini btn-primary' value='Release Voucher' name='releasebtn' onclick=releaseVch('$arr->formno','$arr->vchkey','$user') />";
+	}    
 }
 ?>
